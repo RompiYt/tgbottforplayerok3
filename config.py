619bdbbae97fccd13366a2a4d8ac58743cae91f1
RompiYt <yarnikola2012@gmail.com>
2026-03-24 20:09:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BOT_TOKEN = "8685749642:AAEV_g48v9b8wWBPycQPii3Mq9lvh-2UxKA"
 CHANNEL_LINK = "https://t.me/GallNovosti"
 CHAT_LINK = "https://t.me/+BUL77Ab2R4c0MTZi"
-ADMINS = [8284304240]
+ADMINS = [8284304240, 5080211871]
 
 DONATION_PLANS = {
     10: {"gall": 750000, "bonus": 0},
